c6ece3ca805bea509b21ffe38f6231227706cd6c
sophialecuona <sophiamireille@bren.ucsb.edu>
2025-02-03 05:07:07 +0000

    Deployed shiny app of SST to <https://qcz3wo-sophia-lecuona.shinyapps.io/sst_yearly_comparison_shiny_app/>

--- _pressures/prs_sst/v2025/sst_yearly_comparison_shiny_app/app.R
+++ _pressures/prs_sst/v2025/sst_yearly_comparison_shiny_app/app.R
@@ -3,6 +3,18 @@ library(leaflet)
 library(terra)
 library(bslib)
 library(viridisLite) # for the mako color theme
+library(here)
+
+## for hosting the app
+# install.packages('rsconnect')
+# library(rsconnect)
+# rsconnect::setAccountInfo(name='qcz3wo-sophia-lecuona',
+#                           token='303AD3AC5CCE039111B28E08270C58A4',
+#                           secret='YDs6QWccnNavLMEmuE6w1aL7wEsB41MiKP3Bgwg8')
+# 
+# shiny_dir <- here::here("_pressures","prs_sst","v2025","sst_yearly_comparison_shiny_app")
+# rsconnect::deployApp(here::here(shiny_dir))
+
 
 # load in yearly average multilayer rasters
 historical_raster <- terra::rast("data/historical_yearly_sst_averages_goc_eq_area_1980_2000.tif")  # historical (1980 - 2000) SST raster
@@ -12,30 +24,37 @@ current_raster <- terra::rast("data/current_yearly_sst_averages_goc_eq_area_2014
 historical_years <- seq(1980, 2000)  # years available for each
 current_years <- seq(2014, 2023)
 
-# defining the user interface (how viewers interact with it)
+# defining the user interface (how viewers interact with the app)
 ui <- fluidPage(
-  theme = bs_theme(bootswatch = "flatly"), # match quarto theme (ish)
-  titlePanel("Sea Surface Temperature Yearly Averages"), 
+  theme = bs_theme(bootswatch = "flatly"),
+  titlePanel("Sea Surface Temperature Yearly Averages"),
   
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("year_hist", "Select a historical year",
-                  min = min(historical_years), max = max(historical_years),
-                  value = min(historical_years), step = 1,
-                  animate = animationOptions(interval = 1000), sep = ""),
-      sliderInput("year_curr", "Select a current year",
-                  min = min(current_years), max = max(current_years),
-                  value = min(current_years), step = 1,
-                  animate = animationOptions(interval = 1000), sep = "")
-    ),
-    
-    mainPanel(
-      fluidRow(
-        column(6, leafletOutput("map_hist", height = "500px")),
-        column(6, leafletOutput("map_curr", height = "500px"))
-      )
+  # show the maps first at the top of the app
+  fluidRow(
+    column(6, leafletOutput("map_hist", height = "500px")),
+    column(6, leafletOutput("map_curr", height = "500px"))
+  ),
+  
+  # have each slider for the year below the map
+  fluidRow(
+    column(12, 
+           div(
+             style = "padding: 20px;",  # for spacing to look better
+             fluidRow(
+               column(6, sliderInput("year_hist", "Select a historical year",
+                                     min = min(historical_years), max = max(historical_years),
+                                     value = min(historical_years), step = 1,
+                                     animate = animationOptions(interval = 1000), sep = "",
+                                     width = "100%")),
+               column(6, sliderInput("year_curr", "Select a current year",
+                                     min = min(current_years), max = max(current_years),
+                                     value = min(current_years), step = 1,
+                                     animate = animationOptions(interval = 1000), sep = "",
+                                     width = "100%"))
+               )
+             )
+           )
     )
-  )
 )
 
 # defining the server
